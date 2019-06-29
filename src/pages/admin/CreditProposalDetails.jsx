79f4e341537d4101b24bcdf3afad47cf73ef6fb8
7mioda <ahmededaly1993@gmail.com>
2019-06-30 00:52:06 +0200

    updating credit proposal deatilq

--- src/pages/admin/CreditProposalDetails.jsx
+++ src/pages/admin/CreditProposalDetails.jsx
@@ -3,17 +3,26 @@ import { connect } from 'react-redux';
 import Layout from '../Layout';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
-import { getAllCreditProposals } from '../../actions/creditProposalActions';
+import { getAllCreditProposals, updateCreditProposal } from '../../actions/creditProposalActions';
+import Button from '../../components/Button/Button';
+import { Cell } from '../../components/Table';
 
-const CreditProposalDetails =  ({ getAllCreditProposals, proposal }) => {
+const CreditProposalDetails =  ({ getAllCreditProposals, updateCreditProposal, proposal }) => {
   useEffect(()=> {
     getAllCreditProposals();
     return () => undefined;
   }, []);
-  console.log(proposal);
+  const { client, amount, createdAt ,status, loan } = proposal;
   return(
     <Layout>
-      <p>clientDetails</p>
+      <h2>Détails du demande de crédit</h2>
+      <p>{amount}</p>
+      <p>{createdAt}</p>
+      <p>{status}</p>
+      <p>{loan.name}</p>
+      { !(status === 'approved') && <Button color="#fff" background="#303952" onClick={(event) => { event.stopPropagation(); updateCreditProposal({ ...proposal, status: 'approved', client: client.id, loan: loan.id }); }}>Approuver</Button> }
+      { !(status === 'archived') && <Button color="#fff" background="#303952" onClick={(event) => { event.stopPropagation(); updateCreditProposal({ ...proposal, status: 'archived', client: client.id, loan: loan.id }); }}>Archiver</Button> }
+      { !(status === 'declined') && <Button color="#fff" background="#e66767" onClick={(event) => { event.stopPropagation(); updateCreditProposal({ ...proposal, status: 'declined', client: client.id, loan: loan.id });}}>Réfuser </Button> }
     </Layout>
   );
 };
@@ -22,4 +31,4 @@ const mapStateToProps = (state, { match: { params: { proposalId } }}) => ({
   proposal: state.proposals.proposals.find((proposal) => proposal.id === proposalId ),
 });
 
-export default compose(withRouter, connect(mapStateToProps, { getAllCreditProposals }))(CreditProposalDetails)
+export default compose(withRouter, connect(mapStateToProps, { getAllCreditProposals, updateCreditProposal }))(CreditProposalDetails)
